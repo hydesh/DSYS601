4425da4e4a84019bd2a9acd260517f8bfd567549
hydesh <89956516+hydesh@users.noreply.github.com>
2021-11-28 16:43:56 +1300

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -4,7 +4,7 @@ mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
-cp sample_app.py tempdir/.
+cp assessment_code.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
